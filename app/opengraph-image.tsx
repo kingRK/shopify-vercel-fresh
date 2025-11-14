ca2b9d9fda5041bfbc9643e3972b5f9e5f7b1173
Ui-rk <ui-developer@bvmtechnology.com>
2025-11-15 00:51:04 +0530

    latest opengraph image tsx

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,5 +1,8 @@
 import OpengraphImage from 'components/opengraph-image';
 
+// ADD THIS LINE
+export const dynamic = 'force-dynamic';
+
 export default async function Image() {
   return await OpengraphImage();
 }
